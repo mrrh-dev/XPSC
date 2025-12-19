1d046fb7185d714b82a428818366923499beab56
mrrh-dev <sab.rejwan11@gmail.com>
2025-12-20 00:24:31 +0600

    added a new file random.cpp

---
+++ random.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+#define ll long long 
+template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+int main()
+{
+    ios ::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        
+    }
+    return 0;
+}
